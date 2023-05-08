f40b5c847eff847930ae13fe8dfe55db06badf42
Muhammad Ahsan <m.ahsan@logicps.com>
2023-05-08 16:09:09 +0500

    user route controller implementation.

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -46,5 +46,53 @@ router.post('/sign-up', async (request, response) => {
 
 
 router.post('/sign-in', auth, async (request, response) => {
+    
+    try {
+        
+        const { email, password } = request.body;
+
+        if (!email || !password) {
+            return response.status(400).json({msg: 'Not all fields have been entered.'});
+        }
+
+        const user = await User.findOne({email: email});
+
+        if (!user) {
+            return response.status(400).json({msg: 'Invalid credentials'});
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatch) {
+            return response.status(400).json({msg: 'Invalid credentials'});
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY);
+
+        response.status(200).json({
+            token,
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+            }
+        });
+
+    } catch (error) {
+        response.status(500).json({ msg: error.message });
+    }
+
+});
+
+router.delete('/delete', auth, async (request, response) => {
+
+    try {
+
+        const deletedUser = await User.findByIdAndDelete(request.user);
+        response.status(200).json(deletedUser);
+        
+    } catch (error) {
+        response.status(500).json({ msg: error.message });
+    }
 
 });
